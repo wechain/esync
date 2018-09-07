ab83b7988251e495a8593b9a5bcc5659ff1b38c1
wechain <anybucket@outlook.com>
2018-09-07 19:11:50 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports = {
     options: {
         url: "https://api.steemit.com",
-        db_url: "mongodb://localhost:8882/steem_database",
-        startingBlock: 19323600,
+        db_url: "mongodb://localhost:27017/steem_database",
+        startingBlock: 21323600,
         delayBlocks: 21 //1 min
     }
 };
